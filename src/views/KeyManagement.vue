3a17067fec4f347bf7e46362f456a84fea14264b
Songvut Nakrong <songvut.nakrong@gmail.com>
2021-09-21 21:29:21 +0700

    Initial template views

---
+++ src/views/KeyManagement.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Key Management</h1>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'Home',
+  }
+</script>
---
+++ src/views/RecoverPassword.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Recover Password</h1>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'Recover Password',
+  }
+</script>
