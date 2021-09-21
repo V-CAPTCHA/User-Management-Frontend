f39e493cf38fd5efbf46ac972cffe484571ffbcb
Songvut Nakrong <songvut.nakrong@gmail.com>
2021-09-22 01:27:56 +0700

    Add reset password by token & Update router

---
+++ src/views/ResetPassword.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="reset-password">
+    <center>
+      <h2>Reset Password</h2>
+    </center>
+    <v-card outlined elevation="0" width="480" class="mx-auto my-5 pt-6">
+      <v-form>
+        <v-container>
+          <v-text-field
+            id="newPassword"
+            v-model="newPassword"
+            label="New Password"
+            outlined
+            dense
+          ></v-text-field>
+
+          <v-text-field
+            id="confirmNewPassword"
+            v-model="confirmNewPassword"
+            label="Confirm New Password"
+            outlined
+            dense
+          ></v-text-field>
+          
+          <v-btn 
+            id="loginBtn" 
+            class="mb-6" 
+            color="#1a73e8" 
+            depressed block dark 
+          >
+            Reset Password
+          </v-btn>
+        </v-container>
+      </v-form>
+    </v-card>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'ResetPassword',
+    data() {
+      return {
+        newPassword: '',
+        confirmNewPassword: '',
+      }
+    }
+  }
+</script>
