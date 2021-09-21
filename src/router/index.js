88f47fe9865e35c2a2c05d32da23ed47b1cf26fe
Songvut Nakrong <songvut.nakrong@gmail.com>
2021-09-21 21:46:12 +0700

    Config router all views

--- src/router/index.js
+++ src/router/index.js
@@ -13,11 +13,50 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    component: () => import('../views/About.vue')
+  },
+  {
+    path: '/guide',
+    name: 'Guide',
+    component: () => import('../views/Guide.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: () => import('../views/Register')
+  },
+  {
+    path: '/recoverpassword',
+    name: 'RecoverPassword',
+    component: () => import('../views/RecoverPassword.vue')
+  },
+  {
+    path: '/dashboard',
+    name: 'UserManagement',
+    component: () => import('../views/Home.vue'),
+    children: [
+      {
+        path: '/dashboard',
+        name: 'Dashboard',
+        component: () => import('../views/Dashboard.vue')
+      },
+      {
+        path: '/keymanagement',
+        name: 'KeyManagement',
+        component: () => import('../views/KeyManagement.vue')
+      }
+    ]
+  },
+  {
+    path: '/editprofile',
+    name: 'EditProfile',
+    component: () => import('../views/EditProfile')
+  },
 ]
 
 const router = new VueRouter({
