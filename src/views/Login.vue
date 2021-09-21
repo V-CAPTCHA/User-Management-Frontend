f85b53182b325da509d15a445695927a9b029fbd
Songvut Nakrong <songvut.nakrong@gmail.com>
2021-09-22 01:29:06 +0700

    Update form elements

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,12 +1,67 @@
 <template>
-  <div>
-    <h1>Login</h1>
+  <div class="login">
+    <center>
+      <h2>Login to VCAPTCHA</h2>
+    </center>
+    <v-card outlined elevation="0" width="480" class="mx-auto my-5 pt-6">
+      <v-form>
+        <v-container>
+          <v-text-field
+            id="email"
+            v-model="email"
+            label="Email"
+            outlined
+            dense
+            type="email"
+          ></v-text-field>
+
+          <v-text-field
+            id="password"
+            v-model="password"
+            label="Password"
+            outlined
+            dense
+            type="password"
+          ></v-text-field>
+
+          <router-link id="forgot-password" to="/recoverpassword" class="text-caption">Forgot your password ?</router-link>
+          
+          <v-btn 
+            id="loginBtn" 
+            class="my-6" 
+            color="#1a73e8" 
+            depressed block dark 
+            @click="login()"
+          >
+            Login
+          </v-btn>
+        </v-container>
+      </v-form>
+    </v-card>
+    <center>
+      <span id="registereNow">Don't have an account yet? </span><router-link to="/register">Register now</router-link>
+    </center>
   </div>
 </template>
 
 <script>
 
-  export default {
-    name: 'Login',
+export default {
+  name: 'Login',
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    login: function(){
+      alert('login')
+    }
   }
+}
 </script>
+
+
+<style scoped>
+</style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,6 +1,67 @@
 <template>
   <div>
-    <h1>Register</h1>
+    <center>
+      <h2>Register to VCAPTCHA</h2>
+    </center>
+    <v-card outlined elevation="0" width="480" class="mx-auto my-5 pt-6">
+      <v-form>
+        <v-container>
+          <v-text-field
+            id="first-name"
+            v-model="firstName"
+            label="First Name"
+            outlined
+            dense
+          ></v-text-field>
+
+          <v-text-field
+            id="last-name"
+            v-model="lastName"
+            label="Last Name"
+            outlined
+            dense
+          ></v-text-field>
+
+          <v-text-field
+            id="email"
+            v-model="email"
+            label="Email"
+            outlined
+            dense
+            width="50%"
+            type="email"
+          ></v-text-field>
+          
+          <v-text-field
+            id="password"
+            v-model="password"
+            label="Password"
+            outlined
+            dense
+          ></v-text-field>
+
+          <v-text-field
+            id="confirm-password"
+            v-model="confirmPassword"
+            label="Confirm Password"
+            outlined
+            dense
+          ></v-text-field>
+
+          <v-btn 
+            id="loginBtn" 
+            class="my-6" 
+            color="#1a73e8" 
+            depressed block dark 
+          >
+            Register
+          </v-btn>
+        </v-container>
+      </v-form>
+    </v-card>
+    <center>
+      <span id="registereNow">Already have email and password? </span><router-link to="/login">Login</router-link>
+    </center>
   </div>
 </template>
 
@@ -8,5 +69,14 @@
 
   export default {
     name: 'Register',
+    data() {
+      return {
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+      }
+    }
   }
 </script>
