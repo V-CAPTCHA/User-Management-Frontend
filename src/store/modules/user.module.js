424fb501f120a25d99b3dd070a139962c42212be
Songvut Nakrong <songvut.nakrong@gmail.com>
2021-10-14 01:27:28 +0700

    Add getUserData action in vuex

--- src/store/modules/user.module.js
+++ src/store/modules/user.module.js
@@ -1,11 +1,40 @@
+import axios from "axios"
+
+const API_URL = process.env.VUE_APP_API_URL
+
 export const user = {
   state: {
-
-  },
-  actions: {
-
+    user: "",
   },
   mutations: {
-    
+    user_request(state) {
+      state.status = 'loading'
+    },
+    user_success(state, user) {
+      state.user = user
+    },
+    user_error(state) {
+      state.status = 'error'
+    },
+  },
+  actions: {
+    //get user data
+    getUserData({commit}) {
+      return new Promise((resolve, reject) => {
+        
+        axios.get(API_URL+'/users')
+        .then(res => {
+          let userData = res.data.data;
+          resolve(userData);
+        })
+        .catch(err => {
+          commit('auth_error');
+          reject(err)
+        })
+      })
+    },
+  },
+  getters: {
+    userData: state => state.user
   }
 }
