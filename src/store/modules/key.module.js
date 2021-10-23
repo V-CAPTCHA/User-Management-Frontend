6ba909ed9d5fa66cc11c84af71fa89c74422a21b
Songvut Nakrong <songvut.nakrong@gmail.com>
2021-10-23 17:15:28 +0700

    Add getAllKey action in vuex

--- src/store/modules/key.module.js
+++ src/store/modules/key.module.js
@@ -1,11 +1,36 @@
+import axios from "axios"
+
+const API_URL = process.env.VUE_APP_API_URL
+
+
 export const key = {
   state: {
-
-  },
-  actions: {
-
+    key: []
   },
   mutations: {
-    
-  }
+    key_success(state, key) {
+      state.key = key
+    }
+  },
+  actions: {
+    //get all key
+    getAllKey({commit}) {
+      return new Promise((resolve, reject) => {
+
+        axios.get(API_URL+'/keys')
+        .then(res => {
+          if(res.data.message === "get all keys successfully") {
+            let keys = res.data.data;
+            resolve(keys);
+          }
+          else {
+            reject(res.data.message);
+          }
+        })
+        .catch(err => {
+          reject(err)
+        })
+      }) 
+    },
+  },
 }
