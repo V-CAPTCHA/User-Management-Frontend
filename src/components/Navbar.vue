05e483618aac0e0d25f9c5ad77edd14f32abb24d
Songvut Nakrong <songvut.nakrong@gmail.com>
2021-11-01 19:08:14 +0700

    Fix responsive navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,47 +1,107 @@
 <template>
   <div>
-    <v-app-bar color="#1a73e8" dark>
+    <v-app-bar color="#1a73e8" dark >
       <img src="../assets/logo.png" alt="logo" width="48px"/>
       <v-app-bar-title>
         <v-btn plain :to="{name: 'Home'}"  class="text-h5">VCAPTCHA</v-btn>
       </v-app-bar-title>
 
       <v-spacer></v-spacer>
-      <v-btn plain :to="{name: 'Home'}">Home</v-btn>
-      <v-btn plain :to="{name: 'Guide'}">Guide</v-btn>
-      <v-btn plain href="https://github.com/V-CAPTCHA" target="_blank"><v-icon aria-hidden="false">mdi-github</v-icon> Github</v-btn>
-      
-      <div v-if="!isLoggedIn">
-        <v-btn plain :to="{name: 'Login'}">Login</v-btn>
-        <v-btn plain :to="{name: 'Register'}">Register</v-btn>
+
+      <!-- Hidden only on xs -->
+      <div class="d-none d-sm-flex">
+        <v-btn plain :to="{name: 'Home'}">Home</v-btn>
+        <v-btn plain :to="{name: 'Guide'}">Guide</v-btn>
+        <v-btn plain href="https://github.com/V-CAPTCHA" target="_blank"><v-icon aria-hidden="false">mdi-github</v-icon> Github</v-btn>
+        
+        <div v-if="!isLoggedIn">
+          <v-btn plain :to="{name: 'Login'}">Login</v-btn>
+          <v-btn plain :to="{name: 'Register'}">Register</v-btn>
+        </div>
+
+        <div v-else>
+          <v-menu offset-y>
+            <template v-slot:activator="{ on, attrs}">
+              <v-btn plain v-on="on" v-bind="attrs">
+                Profile<v-icon aria-hidden="false">mdi-chevron-down</v-icon>
+              </v-btn>
+            </template>
+
+            <v-list>
+              <v-list-item>
+                <v-btn plain :to="{name: 'Dashboard'}">Dashboard</v-btn>
+              </v-list-item>
+              <v-list-item>
+                <v-btn plain :to="{name: 'KeyManagement'}">Key Management</v-btn>
+              </v-list-item>
+              <v-list-item>
+                <v-btn plain :to="{name: 'EditProfile'}">Edit Profile</v-btn>
+              </v-list-item>
+              <v-list-item>
+                <v-btn text plain @click="logOut()">Logout</v-btn>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </div>
       </div>
 
-      <div v-else>
-        <v-menu offset-y>
-          <template v-slot:activator="{ on, attrs}">
-            <v-btn plain v-on="on" v-bind="attrs">
-              Profile<v-icon aria-hidden="false">mdi-chevron-down</v-icon>
-            </v-btn>
-          </template>
-
-          <v-list>
-            <v-list-item>
-              <v-btn plain :to="{name: 'Dashboard'}">Dashboard</v-btn>
-            </v-list-item>
-            <v-list-item>
-              <v-btn plain :to="{name: 'KeyManagement'}">Key Management</v-btn>
-            </v-list-item>
-            <v-list-item>
-              <v-btn plain :to="{name: 'EditProfile'}">Edit Profile</v-btn>
-            </v-list-item>
-            <v-list-item>
-              <v-btn text plain @click="logOut()">Logout</v-btn>
-            </v-list-item>
-          </v-list>
-
-        </v-menu>
-      </div>
+      <!-- Visible only on xs	-->
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" class="d-flex d-sm-none"></v-app-bar-nav-icon>
     </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      right
+      temporary
+      :width="512"
+    >
+      <v-list
+        nav
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="blue--text text--accent-4"
+        >
+          <v-list-item :to="{name: 'Home'}">
+            <v-list-item-title >Home</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item :to="{name: 'Guide'}">
+            <v-list-item-title>Guide</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item href="https://github.com/V-CAPTCHA" target="_blank">
+            <v-list-item-title>Github <v-icon aria-hidden="false">mdi-github</v-icon></v-list-item-title>
+          </v-list-item>
+
+          <div v-if="!isLoggedIn">
+            <v-list-item :to="{name: 'Login'}">
+              <v-list-item-title>Login</v-list-item-title>
+            </v-list-item>
+
+            <v-list-item :to="{name: 'Register'}">
+              <v-list-item-title>Register</v-list-item-title>
+            </v-list-item>
+          </div>
+
+          <div v-else>
+            <v-list-item :to="{name: 'Dashboard'}">
+              <v-list-item-title>Dashboard</v-list-item-title>
+            </v-list-item>
+            <v-list-item :to="{name: 'KeyManagement'}">
+              <v-list-item-title>Key Management</v-list-item-title>
+            </v-list-item>
+            <v-list-item :to="{name: 'EditProfile'}">
+              <v-list-item-title>Edit Profile</v-list-item-title>
+            </v-list-item>
+            <v-list-item @click="logOut()">
+              <v-list-item-title>Logout</v-list-item-title>
+            </v-list-item>
+          </div>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
 
     <v-progress-linear
       v-if="isLoading"
@@ -60,6 +120,12 @@
 <script>
 export default {
   name: 'Navbar',
+  data() {
+    return {
+      drawer: false,
+      group: null,
+    }
+  },
   methods: {
     logOut: function() {
       this.$store.dispatch('logout')
@@ -73,7 +139,12 @@ export default {
     isLoading: function() {
       return this.$store.getters.authStatus === 'loading'
     }
-  }
+  },
+  watch: {
+    group() {
+      this.drawer = false
+    },
+  },
 }
 </script>
 
