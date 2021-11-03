f12c3cc777013216e3de441374350349555bdebb
Songvut Nakrong <songvut.nakrong@gmail.com>
2021-11-03 21:32:15 +0700

    Fix label not correct with data

--- src/util/generateDateLabel.js
+++ src/util/generateDateLabel.js
@@ -3,7 +3,7 @@ const moment = require('moment');
 export function generateDateLabel() {
   var dateLabel = [];
   
-  for(var i=-90; i<=0; i++) {
+  for(var i=-89; i<0; i++) {
     dateLabel.push(moment().add(i, 'days').format('DD-MM-YY'))
   }  
 
