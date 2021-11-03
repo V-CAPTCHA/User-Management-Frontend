132de771c0f3a68d0f62d62fd1ff45517b2ec99d
Songvut Nakrong <songvut.nakrong@gmail.com>
2021-11-03 18:34:34 +0700

    Update icon github button

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -12,7 +12,11 @@
       <div class="d-none d-sm-flex">
         <v-btn plain :to="{name: 'Home'}">Home</v-btn>
         <v-btn plain :to="{name: 'Guide'}">Guide</v-btn>
-        <v-btn plain href="https://github.com/V-CAPTCHA" target="_blank"><v-icon aria-hidden="false">mdi-github</v-icon> Github</v-btn>
+        <v-btn plain href="https://github.com/V-CAPTCHA" target="_blank">
+          <v-icon aria-hidden="false" dense>mdi-github</v-icon> 
+          Github
+          <v-icon aria-hidden="false" small>mdi-open-in-new</v-icon>
+        </v-btn>
         
         <div v-if="!isLoggedIn">
           <v-btn plain :to="{name: 'Login'}">Login</v-btn>
