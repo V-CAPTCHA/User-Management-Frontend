f816b301bb04e9b2f5dcf94d4ba93ccfdc64b1a4
Songvut Nakrong <songvut.nakrong@gmail.com>
2021-11-18 14:30:43 +0700

    Update content of guide and Add skeleton instead image

--- src/views/Guide.vue
+++ src/views/Guide.vue
@@ -7,32 +7,71 @@
           <h1>Guide</h1>
           <br>
 
+          <!-- Introduction -->
           <div id="introduction" ref="introduction">
             <h2>Introduction</h2>
             <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. At accusantium nobis, illo, ad fugiat nisi velit voluptatem autem saepe accusamus reprehenderit alias exercitationem excepturi laborum fuga ducimus eum quaerat. Odit.
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. A voluptatem blanditiis soluta, neque eius, in autem voluptatibus possimus sunt voluptates unde aliquid facilis perspiciatis earum modi quas sint incidunt labore.
+              Human Verification Service using Voice Recognition The purpose is to create a system to determine that users are human instead of using images. 
+              As an alternative way to test whether the user is human or not. By displaying a picture or story and letting users speak what they see. 
+              The system then determines whether the user interaction is human or not.
             </p>
+            <v-skeleton-loader type="image"></v-skeleton-loader>
           </div>
           <hr>
-
+          
+          <!-- Widget -->
           <div id="widget">
             <h2 ref="vcaptcha-widget">VCAPTCHA Widget</h2>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis iusto laborum ullam veniam reprehenderit beatae dolorem sit explicabo officia odio obcaecati placeat magnam corporis, architecto facere hic cumque blanditiis nam.
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe eligendi harum totam magni quam aspernatur dolorem distinctio maiores, sint eum earum magnam assumenda hic aliquid, ipsa eaque explicabo est voluptas!
+              The way to use VCAPTCHA in your web pages is add a javascript file to your HTML file. 
             </p>
-            
+
+            <p>How to get KEY ID:</p>
+            <ol class="ml-5">
+              <li>Go to <a href="https://vcaptcha.work" target="_blank">https://vcaptcha.work</a></li>
+              <li>Register and Login</li>
+              <li>
+                Go to <a href="https://vcaptcha.work/keymanagement" target="_blank">KEY MANAGEMENT</a> or 
+                <a href="https://vcaptcha.work/keymanagement" target="_blank">KEYS</a> tab
+              </li>
+              <li>Click at create key</li>
+              <li>Copy your Key ID to your widget</li>
+            </ol>
+            <br>
+                        
             <h3 ref="widget">Widget</h3>
-            <div class="code-box"></div>
+            <ol class="ml-5">
+              <li>Load the JavaScript file</li>
+              <v-skeleton-loader type="image"></v-skeleton-loader>
+              <br>
+
+              <li>Add function to your script tag to handle vcaptcha key</li>
+              <v-skeleton-loader type="image"></v-skeleton-loader>
+              <br>
+
+              <li>Add VCAPTCHA element to your web page</li>
+              <v-skeleton-loader type="image"></v-skeleton-loader>
+              <br>
+
+            </ol>
             <br>
 
-            <h3 ref="configuration">Configuration</h3>
-            <div class="code-box"></div>
+            <!-- Site Verify Response -->
+            <h3 ref="site-verify-response">Site verify response</h3>
+            <p>Make the request to verify the response token. The response is a JSON object:</p>
+            <v-skeleton-loader type="image"></v-skeleton-loader>
             <br>
+
+            <!-- Configuration -->
+            <h3 ref="configuration">Configuration</h3>
+            <p>You can config your VCAPTCHA widget following this:</p>
+            <v-skeleton-loader type="image"></v-skeleton-loader>
+            <br>
+
           </div>
           <hr>
 
+          <!-- Server Side
           <div id="server-side">
             <h2 ref="server-side">Server Side</h2>
             <p>
@@ -68,8 +107,7 @@
               </tbody>
             </v-simple-table>
           </div>
-          <hr>
-
+          <hr> -->
 
         </v-col>
         
@@ -82,12 +120,13 @@
               <li @click="goto('vcaptcha-widget')">VCAPTCHA Widget</li>
               <ul>
                 <li @click="goto('widget')">Widget</li>
+                <li @click="goto('site-verify-response')">Site Verify Response</li>
                 <li @click="goto('configuration')">Configuration</li>
               </ul>
-              <li @click="goto('server-side')">Server Side</li>
+              <!-- <li @click="goto('server-side')">Server Side</li>
               <ul>
                 <li @click="goto('api')">API</li>
-              </ul>
+              </ul> -->
             </ul>
           </div>
         </v-col>
@@ -122,7 +161,7 @@ hr {
 }
 
 .code-box {
-  background-color: grey;
+  background-color: lightgrey;
   width: 100%;
   height: 200px;
 }
