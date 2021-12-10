6a0fdfbe274bee1c7d019ce80f21d5a9bb89500a
Songvut Nakrong <songvut.nakrong@gmail.com>
2021-12-10 14:47:42 +0700

    Fix date label not show current date

--- src/util/generateDateLabel.js
+++ src/util/generateDateLabel.js
@@ -3,7 +3,7 @@ const moment = require('moment');
 export function generateDateLabel() {
   var dateLabel = [];
   
-  for(var i=-89; i<0; i++) {
+  for(var i=-89; i<1; i++) {
     dateLabel.push(moment().add(i, 'days').format('DD-MM-YY'))
   }  
 
