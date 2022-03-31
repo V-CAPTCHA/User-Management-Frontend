9507dedcb4b68fb31ef4a7ef71b42ec3c1aa0f2a
THANATATH <thanatuth.euro@gmail.com>
2022-03-31 17:07:02 +0700

    hotfix-auth-err_message

--- src/store/modules/auth.module.js
+++ src/store/modules/auth.module.js
@@ -57,8 +57,15 @@ export const auth = {
           }
         })
         .catch((err) => {
+          
+          var err_text = 'Something went wrong'
+          
+          console.log(err.response.data.errors)
+          if (err.response.data.errors != undefined){err_text= err.response.data.errors[0].msg}else{
+            err_text= err.response.data.message
+          }
           let payload = {
-            text: err.response.data.errors[0].msg || err.response.data.message,
+            text: err_text,
             snackbar: true
           }
           commit('updateSnackbar', payload, {root: true})
